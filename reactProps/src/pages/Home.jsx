bd9dc66076887e9aedd8ab058f50dcaaed75962e
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-03-18 11:40:23 +0500

    react things

---
+++ reactProps/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Greetings from '../components/Greetings'
+
+const Home = () => {
+  return (
+    <div>
+      <Greetings name="Affan" />
+    </div>
+  )
+}
+
+export default Home
