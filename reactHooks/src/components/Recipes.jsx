ca684ea67d56b032a5f7a5e04406dea1da121338
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-03-28 19:51:14 +0500

    Hooks

--- reactHooks/src/components/Recipes.jsx
+++ reactHooks/src/components/Recipes.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect, useState } from "react";
 const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    fetch("https://dummyjson.com/recipes")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data));
+  }, []);
   return (
     <div>
-      
+      <div className="row">
+        {recipes.map((recipe) => (
+          <div className="col-4 mb-3" key={recipe.id}>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src={recipe.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{recipe.name}</h5>
+                {recipe.instructions.map((ins, i) => (
+                  <p className="card-text" key={i}>
+                    {ins}
+                  </p>
+                ))}
+                <a href="#" className="btn btn-dark">
+                  Order Now
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Recipes
+export default Recipes;
